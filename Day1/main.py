a646e9c7dde1867f81ad3d21cc1f4b0183a2c3ad
Jeroen Craps <jeroen.craps@froomle.com>
2020-12-01 10:00:35 +0100

    Day 1 - Python solution

---
+++ Day1/main.py
@@ -0,0 +1,22 @@
+def find_pair(target, arr):
+    s = set()
+    for num in arr:
+        if (target - num) in s:
+            return (num, target - num)
+        else:
+            s.add(num)
+    return None, None
+
+
+with open("./input") as f:
+    arr = [int(i) for i in f]
+    target = 2020
+
+    a, b = find_pair(target, arr)
+    print(f"{a} + {b} = {a+b} \nResult: {a * b}")
+
+    for idx, a in enumerate(arr):
+        b, c = find_pair(target - a, arr[idx:])
+        if a and b:
+            print(f"{a} + {b} + {c} = {a + b + c} \nResult: {a * b * c}")
+            break
